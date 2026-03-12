bbea4100d9a931ec91b90ee30993b7b3702edb04
Kaito Watanabe <kaitowatanabemcc@gmail.com>
2026-03-12 20:16:42 +1100

    add backdrop to navbar on scroll

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,23 @@
 import ThemeToggle from "../features/theme/ThemeToggle";
 import { playSwitchSound } from "../assets/audio/switch";
+import { useEffect, useState } from "react";
 
 function Navbar({ handleThemeToggle, isDark }) {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    function onScroll() {
+      setScrolled(window.scrollY > 40);
+    }
+    onScroll();
+
+    // run onScroll as the window is being scrolled
+    // passive:true stops preventDefault() to make scrolling smoother
+    window.addEventListener("scroll", onScroll, { passive: true });
+  }, []);
+
   return (
-    <header className="nav-container">
+    <header className={`nav-container ${scrolled ? "nav-scrolled" : ""}`}>
       <a className="logo" href="#home" onClick={playSwitchSound}>
         KaitoWatanabe
       </a>
